c03e0edd472e114e1d31cce23298b0910301ec05
Ratan Gulati <ratangulati.dev@gmail.com>
2026-04-21 16:07:39 +0530

    style: fix body bg token conflict and tokenize grey values

--- app/layout.tsx
+++ app/layout.tsx
@@ -29,7 +29,7 @@ export default function RootLayout({
       lang="en"
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
-      <body className="min-h-full flex flex-col bg-black text-white">
+      <body className="min-h-full flex flex-col bg-[#0a0a0a] text-white">
 
         {/* ✅ FIX: wrap with Providers */}
         <Providers>
